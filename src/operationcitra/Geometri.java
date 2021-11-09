dd39e3bde25a7afd028eeb21333fd2b8a627770f
erisumea <09021281924061@student.unsri.ac.id>
2021-11-09 07:44:50 +0700

    update geometri translasi, flipping

--- src/operationcitra/Geometri.java
+++ src/operationcitra/Geometri.java
@@ -15,6 +15,42 @@ import javax.imageio.ImageIO;
  * @author Adit,Diaz,Angga
  */
 public class Geometri {
+    public static void citraTranslasi (String nama_file1, String tipe_file, int x) {
+        try{
+            String input_name = nama_file1 + "." + tipe_file;
+            File input = new File(input_name);
+            BufferedImage image = ImageIO.read(input);
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            BufferedImage image2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            int height2 = image2.getHeight();
+            int width2 = image2.getWidth();
+            int k = 0;
+            for(int i = 0; i < height2; i++){
+                int l = 0;
+                for(int j = 0; j < width2; j++){
+                    if(i < x || j < x ){
+                        Color c = new Color(0, 0, 0);
+                        image2.setRGB(j, i, c.getRGB());
+                    }
+                    else {
+                        Color c = new Color(image.getRGB(l, k));
+                        image2.setRGB(j, i, c.getRGB());
+                        l++;
+                        if (l >= (width - (x))) k++;
+                    }
+                }
+            }
+
+            String output_name = nama_file1 + "_Translasi." + tipe_file;
+            File output = new File(output_name);
+            ImageIO.write(image2, tipe_file, output);
+        } catch(IOException e) {
+            System.out.println("Error = " + e);
+        }
+    }
+
     public static void citraRotasi90CW(String nama_file, String nama_file2, String tipe_file) {
         try {
             String input_name = nama_file + "." + tipe_file;
@@ -65,4 +101,55 @@ public class Geometri {
         }
     }
 
+    public static void citraHorizontalFlip (String nama_file1, String tipe_file) {
+        try{
+            String input_name = nama_file1 + "." + tipe_file;
+            File input = new File(input_name);
+            BufferedImage image = ImageIO.read(input);
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            BufferedImage image2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            for(int i = 0; i < height; i++){
+                for(int j = 0; j < width; j++){
+                    int k = width - 1;
+                    int p = image.getRGB(j, i);
+                    image2.setRGB(k, i, p);
+                    k--;
+                }
+            }
+
+            String output_name = nama_file1 + "_HorizontalFlip." + tipe_file;
+            File output = new File(output_name);
+            ImageIO.write(image2, tipe_file, output);
+        } catch(IOException e) {
+            System.out.println("Error = " + e);
+        }
+    }
+
+    public static void citraVerticalFlip (String nama_file1, String tipe_file) {
+        try{
+            String input_name = nama_file1 + "." + tipe_file;
+            File input = new File(input_name);
+            BufferedImage image = ImageIO.read(input);
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            BufferedImage image2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            int k = width - 1;
+            for(int i = 0; i < height; i++){
+                for(int j = 0; j < width; j++){
+                    int p = image.getRGB(j, k);
+                    image2.setRGB(j, i, p);
+                }
+                k--;
+            }
+
+            String output_name = nama_file1 + "_VerticalFlip." + tipe_file;
+            File output = new File(output_name);
+            ImageIO.write(image2, tipe_file, output);
+        } catch(IOException e) {
+            System.out.println("Error = " + e);
+        }
+    }
 }
